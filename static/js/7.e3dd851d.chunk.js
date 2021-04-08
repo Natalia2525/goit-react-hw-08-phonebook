6211f364ff3494fbc2665fdc059e0a061344622b
Natalia2525 <65954006+Natalia2525@users.noreply.github.com>
2021-04-09 00:25:31 +0300

    Updates

---
+++ static/js/7.e3dd851d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[7],{88:function(t,a,e){"use strict";e.r(a);var o=e(1);a.default=function(){return Object(o.jsxs)("div",{children:[Object(o.jsx)("h1",{children:" \u0413\u043b\u0430\u0432\u043d\u0430\u044f \u0441\u0442\u0440\u0430\u043d\u0438\u0446\u0430"}),Object(o.jsx)("img",{src:"https://habrastorage.org/web/0a3/ab4/d74/0a3ab4d74aa04b0ab913569836ea4208.jpg",alt:"book"})]})}}}]);
+//# sourceMappingURL=7.e3dd851d.chunk.js.map
